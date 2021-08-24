8a16f1dcaa0bc3253c07f990fe1c15d3156ac2c2
João Victor <joaovictor.cogomes@gmail.com>
2021-08-24 18:00:48 -0300

    Tratando o status de retorno na busca de uma cozinha por código

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +21,27 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
+
 	@GetMapping("/{codigo}")
-	public Cozinha buscar(@PathVariable Long codigo) {
-		return cozinhaRepository.buscar(codigo);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long codigo) {
+		Cozinha cozinha = cozinhaRepository.buscar(codigo);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
 	}
-	
+
 }
