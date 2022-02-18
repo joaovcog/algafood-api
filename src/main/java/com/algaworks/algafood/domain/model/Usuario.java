66fdbaa69c839712508ff1c19b98afc5c4528c0e
Joao Victor <joaovictor.cogomes@gmail.com>
2022-02-18 16:56:39 -0300

    Criando e mapeando entidades Grupo e Usuario

---
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String senha;
+
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+
+	@ManyToMany
+	@JoinTable(name = "usuarios_grupos", 
+		joinColumns = @JoinColumn(name = "cod_usuario"), 
+		inverseJoinColumns = @JoinColumn(name = "cod_grupo"))
+	private List<Grupo> grupos = new ArrayList<>();
+
+}
