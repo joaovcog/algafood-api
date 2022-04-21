49dff77378c1cd2c0b73e52ea5d722251e5446f8
joaovcog <joaovictor.cogomes@gmail.com>
2022-04-21 19:14:42 -0300

    Gerando relatorio de vendas diarias

--- src/main/java/com/algaworks/algafood/infrastructure/service/report/VendaReportServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/report/VendaReportServiceImpl.java
@@ -1,16 +1,51 @@
 package com.algaworks.algafood.infrastructure.service.report;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.dto.VendaDiaria;
+import com.algaworks.algafood.domain.repository.EstatisticaVendaRepository;
 import com.algaworks.algafood.domain.service.VendaReportService;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class VendaReportServiceImpl implements VendaReportService {
 
+	@Autowired
+	private EstatisticaVendaRepository estatisticaVendaRepository;
+
 	@Override
 	public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
-		return null;
+		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+		List<VendaDiaria> vendasDiarias = estatisticaVendaRepository.consultarVendasDiarias(filtro, timeOffset);
+		JRDataSource dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+		JasperPrint jasperPrint = null;
+
+		try {
+			jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+
+			return JasperExportManager.exportReportToPdf(jasperPrint);
+		} catch (JRException e) {
+			throw new ReportException("Não foi possível emitir relatório de vendas diárias.", e);
+		}
 	}
 
 }
