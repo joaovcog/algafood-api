b222286b1cee29dffea14e2dfef56ae62fb2d43c
joaovcog <joaovictor.cogomes@gmail.com>
2022-04-22 11:52:57 -0300

    Criacao de input para requisicao da foto e validacao tamanho arquivo

--- src/main/java/com/algaworks/algafood/api/dto/input/FotoProdutoInputDto.java
+++ src/main/java/com/algaworks/algafood/api/dto/input/FotoProdutoInputDto.java
@@ -1,6 +1,11 @@
 package com.algaworks.algafood.api.dto.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +14,11 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInputDto {
 	
+	@NotNull
+	@FileSize(max = "100KB")
 	private MultipartFile arquivo;
+	
+	@NotBlank
 	private String descricao;
 	
 }
