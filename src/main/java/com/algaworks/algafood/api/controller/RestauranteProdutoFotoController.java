0bf0b3179a15764218d17f696c2bb3ce277fa7a2
joaovcog <joaovictor.cogomes@gmail.com>
2022-04-22 10:57:32 -0300

    Criando endpoint para upload de arquivo com MultipartFile

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.api.controller;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.dto.input.FotoProdutoInputDto;
+
+@RestController
+@RequestMapping("/restaurantes/{codRestaurante}/produtos/{codProduto}/foto")
+public class RestauranteProdutoFotoController {
+
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void atualizarFoto(@PathVariable Long codRestaurante, @PathVariable Long codProduto,
+			FotoProdutoInputDto fotoProdutoInput) {
+		String nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+		
+		Path pathArquivoFoto = Path.of("C:\\Users\\joaov\\Pictures\\catalogo", nomeArquivo);
+		
+		System.out.println(fotoProdutoInput.getDescricao());
+		System.out.println(pathArquivoFoto);
+		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		
+		try {
+			fotoProdutoInput.getArquivo().transferTo(pathArquivoFoto);
+		} catch (IllegalStateException | IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
