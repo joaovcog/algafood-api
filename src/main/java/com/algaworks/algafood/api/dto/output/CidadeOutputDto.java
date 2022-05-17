8c0c137f8a4fa8cb69de55d7a97aaba13f29a480
joaovcog <joaovictor.cogomes@gmail.com>
2022-05-17 14:56:39 -0300

    Descrevendo modelos de representacao e propriedades

--- src/main/java/com/algaworks/algafood/api/dto/output/CidadeOutputDto.java
+++ src/main/java/com/algaworks/algafood/api/dto/output/CidadeOutputDto.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood.api.dto.output;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+//@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeOutputDto {
 	
+	//@ApiModelProperty(value = "Código da cidade", example = "1")
+	@ApiModelProperty(example = "1")
 	private Long codigo;
+	
+	@ApiModelProperty(example = "Itumbiara")
 	private String nome;
 	private EstadoOutputDto estado;
 	
--- src/main/java/com/algaworks/algafood/api/dto/output/EstadoOutputDto.java
+++ src/main/java/com/algaworks/algafood/api/dto/output/EstadoOutputDto.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.dto.output;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Setter
 public class EstadoOutputDto {
 	
+	@ApiModelProperty(example = "1")
 	private Long codigo;
+	
+	@ApiModelProperty(example = "Goiás")
 	private String nome;
 	
 }
