2b0a46d8cebd1a5ae05c94e8fe3fc20e2239bb4a
joaovcog <joaovictor.cogomes@gmail.com>
2022-05-17 14:22:05 -0300

    Customizando informacoes da API na documentacao

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.algaworks.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,7 +21,17 @@ public class SpringFoxConfig {
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 					.paths(PathSelectors.any())
 					//.paths(PathSelectors.ant("/restaurantes/*"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("AlgaFood API")
+				.description("API desenvolvida no curso ESR da AlgaWorks")
+				.version("1")
+				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
+				.build();
 	}
 	
 }
