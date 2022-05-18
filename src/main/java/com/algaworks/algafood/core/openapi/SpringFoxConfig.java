f95c78dd35964e4738b0f9622a4d1a29dc6d5c4f
joaovcog <joaovictor.cogomes@gmail.com>
2022-05-18 12:09:43 -0300

    Incluindo a representacao de ApiError na documentacao

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,14 +2,21 @@ package com.algaworks.algafood.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.algaworks.algafood.api.exceptionhandler.ApiError;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -17,6 +24,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -24,8 +32,11 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
+		TypeResolver typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.OAS_30).select()
-					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")).paths(PathSelectors.any())
+					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+					.paths(PathSelectors.any())
 					// .paths(PathSelectors.ant("/restaurantes/*"))
 					.build()
 				.useDefaultResponseMessages(false)
@@ -33,8 +44,14 @@ public class SpringFoxConfig {
 				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
 				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
 				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+				.additionalModels(typeResolver.resolve(ApiError.class))
 				.apiInfo(apiInfo())
 				.tags(tags()[0], tags());
+	}
+	
+	@Bean
+	public JacksonModuleRegistrar springFoxJacksonConfig() {
+		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
 
 	private ApiInfo apiInfo() {
@@ -46,7 +63,10 @@ public class SpringFoxConfig {
 	private List<Response> globalGetResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno do Servidor").build(),
+						.description("Erro interno do Servidor")
+						.representation(MediaType.APPLICATION_JSON)
+						.apply(getApiErrorModelReference())
+						.build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
 						.description("Recurso não possui representação que pode ser aceita pelo consumidor").build()
 				);
@@ -55,13 +75,23 @@ public class SpringFoxConfig {
 	private List<Response> globalPostPutResponseMessages() {
 		return Arrays.asList(
 				new ResponseBuilder().code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-						.description("Requisição inválida (erro do cliente)").build(),
+						.description("Requisição inválida (erro do cliente)")
+						.representation(MediaType.APPLICATION_JSON)
+						.apply(getApiErrorModelReference())
+						.build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-						.description("Erro interno no servidor").build(),
+						.description("Erro interno no servidor")
+						.representation(MediaType.APPLICATION_JSON)
+						.apply(getApiErrorModelReference())
+						.build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-						.description("Recurso não possui representação que poderia ser aceita pelo consumidor").build(),
+						.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build(),
 				new ResponseBuilder().code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-						.description("Requisição recusada porque o corpo está em um formato não suportado").build()
+						.description("Requisição recusada porque o corpo está em um formato não suportado")
+						.representation(MediaType.APPLICATION_JSON)
+						.apply(getApiErrorModelReference())
+						.build()
 				);
 	}
 	
@@ -72,6 +102,12 @@ public class SpringFoxConfig {
 				new ResponseBuilder().code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
 						.description("Erro interno no servidor").build()
 				);
+	}
+	
+	private Consumer<RepresentationBuilder> getApiErrorModelReference() {
+	    return r -> r.model(m -> m.name("ApiError")
+	            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+	                    q -> q.name("ApiError").namespace("com.algaworks.algafood.api.exceptionhandler")))));
 	}
 
 	private Tag[] tags() {
