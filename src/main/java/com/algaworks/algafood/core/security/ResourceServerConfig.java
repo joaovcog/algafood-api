1c61d03fdc4c9d270bdf00a0c236b13dcd4d1ed1
joaovcog <joaovictor.cogomes@gmail.com>
2022-07-04 18:39:02 -0300

    Configurando a chave publica no resource server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,15 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -25,13 +19,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.oauth2ResourceServer()
 				.jwt();
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		SecretKey secret = new SecretKeySpec("AEAEeaae87968564ea4ae6a5e4ea6ee6ADSasD6546SD546A".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secret).build();
 	}
 	
 }
