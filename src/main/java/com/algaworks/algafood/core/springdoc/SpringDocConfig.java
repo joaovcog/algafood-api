b8521a59e4d5ff7bdf79478230ce572a301bfdfc
joaovcog <joaovictor.cogomes@gmail.com>
2022-08-30 20:25:14 -0300

    Configurando Springdoc

---
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.core.springdoc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SpringDocConfig {
+	
+	@Bean
+	public OpenAPI openApi() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("AlgaFood API")
+						.version("v1")
+						.description("REST API do AlgaFood")
+						.license(new License()
+								.name("Apache 2.0")
+								.url("http://springdoc.com")
+						)
+				);
+	}
+	
+}
