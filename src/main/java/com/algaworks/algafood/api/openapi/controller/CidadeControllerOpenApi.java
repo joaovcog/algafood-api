57700112c28e8d6e041f17cd0f281b2b7ee62c92
joaovcog <joaovictor.cogomes@gmail.com>
2022-08-31 18:58:40 -0300

    Descrevendo tags, operações e parâmetros de entrada para Cidades

--- src/main/java/com/algaworks/algafood/api/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/CidadeControllerOpenApi.java
@@ -6,19 +6,30 @@ import org.springframework.hateoas.PagedModel;
 import com.algaworks.algafood.api.dto.input.CidadeInputDto;
 import com.algaworks.algafood.api.dto.output.CidadeOutputDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security-auth")
+@Tag(name = "Cidades", description = "Gerencia as cidades")
 public interface CidadeControllerOpenApi {
 	
+	@Operation(summary = "Lista as cidades")
 	public PagedModel<CidadeOutputDto> listar(Pageable pageable);
 
-	public CidadeOutputDto buscar(Long codCidade);
+	@Operation(summary = "Busca uma cidade por código")
+	public CidadeOutputDto buscar(@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade);
 
-	public CidadeOutputDto adicionar(CidadeInputDto cidadeInput);
+	@Operation(summary = "Adiciona uma nova cidade", description = "Cadastro de uma cidade necessita de um estado e um nome válido")
+	public CidadeOutputDto adicionar(@RequestBody(description = "Representação de uma nova cidade", required = true) CidadeInputDto cidadeInput);
 
-	public CidadeOutputDto atualizar(Long codCidade, CidadeInputDto cidadeInput);
+	@Operation(summary = "Atualiza uma cidade por código")
+	public CidadeOutputDto atualizar(@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade, 
+			@RequestBody(description = "Representação de uma cidade com dados atualizados", required = true) CidadeInputDto cidadeInput);
 
-	public void remover(Long codCidade);
+	@Operation(summary = "Remove uma cidade por código")
+	public void remover(@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade);
 	
 }
