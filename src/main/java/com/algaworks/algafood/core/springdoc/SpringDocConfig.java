b0b0b7e99ac879788648131ab20be3cf68ccfe44
joaovcog <joaovictor.cogomes@gmail.com>
2022-09-01 19:20:23 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.springdoc;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 
 @Configuration
 @SecurityScheme(name = "security-auth", 
@@ -39,6 +42,28 @@ public class SpringDocConfig {
 								.url("http://springdoc.com")
 						)
 				);
+	}
+	
+	@Bean
+	public OpenApiCustomiser openApiCustomiser() {
+		return openApi -> { 
+			openApi.getPaths()
+				.values()
+				.stream()
+				.flatMap(pathItem -> pathItem.readOperations().stream())
+				.forEach(operation -> {
+					ApiResponses responses = operation.getResponses();
+					
+					ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso não encontrado");
+					ApiResponse apiResponseSemRepresentacao = new ApiResponse().description("Recurso não possui uma representação que poderia ser aceita pelo consumidor");
+					ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
+					
+					responses.addApiResponse("404", apiResponseNaoEncontrado);
+					responses.addApiResponse("406", apiResponseSemRepresentacao);
+					responses.addApiResponse("500", apiResponseErroInterno);
+				});
+				
+		};
 	}
 	
 }
