9b0bfdcaa4b38e2fff949add78beb5c118d3511b
joaovcog <joaovictor.cogomes@gmail.com>
2022-09-04 11:00:36 -0300

    Descrevendo documentação de endpoints de grupos

--- src/main/java/com/algaworks/algafood/api/dto/output/GrupoOutputDto.java
+++ src/main/java/com/algaworks/algafood/api/dto/output/GrupoOutputDto.java
@@ -1,14 +1,20 @@
 package com.algaworks.algafood.api.dto.output;
 
+import org.springframework.hateoas.server.core.Relation;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "grupos")
 @Getter
 @Setter
 public class GrupoOutputDto {
 	
+	@Schema(example = "1")
 	private Long codigo;
 	
+	@Schema(example = "Administrador")
 	private String nome;
 	
 }
