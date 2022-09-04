34651545ba2b7de6ad1d4703125bb718ffaf9e93
joaovcog <joaovictor.cogomes@gmail.com>
2022-09-04 11:18:35 -0300

    Descrevendo documentacao para Pageable

--- src/main/java/com/algaworks/algafood/api/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/CidadeControllerOpenApi.java
@@ -5,6 +5,7 @@ import org.springframework.hateoas.PagedModel;
 
 import com.algaworks.algafood.api.dto.input.CidadeInputDto;
 import com.algaworks.algafood.api.dto.output.CidadeOutputDto;
+import com.algaworks.algafood.core.springdoc.PageableParameter;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -20,45 +21,29 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface CidadeControllerOpenApi {
 
 	@Operation(summary = "Lista as cidades")
-	public PagedModel<CidadeOutputDto> listar(Pageable pageable);
+	@PageableParameter
+	public PagedModel<CidadeOutputDto> listar(@Parameter(hidden = true) Pageable pageable);
 
-	@Operation(summary = "Busca uma cidade por código", responses = {
-			@ApiResponse(responseCode = "200"), 
-			@ApiResponse(responseCode = "400", 
-				description = "Código da cidade inválido", 
-				content = @Content(schema = @Schema(ref = "ApiError"))), 
-			@ApiResponse(responseCode = "404", 
-				description = "Cidade não encontrada", 
-				content = @Content(schema = @Schema(ref = "ApiError")))
-		})
-	public CidadeOutputDto buscar(@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade);
+	@Operation(summary = "Busca uma cidade por código", responses = { @ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "Código da cidade inválido", content = @Content(schema = @Schema(ref = "ApiError"))),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(ref = "ApiError"))) })
+	public CidadeOutputDto buscar(
+			@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade);
 
 	@Operation(summary = "Adiciona uma nova cidade", description = "Cadastro de uma cidade necessita de um estado e um nome válido")
 	public CidadeOutputDto adicionar(
 			@RequestBody(description = "Representação de uma nova cidade", required = true) CidadeInputDto cidadeInput);
 
-	@Operation(summary = "Atualiza uma cidade por código", responses = {
-			@ApiResponse(responseCode = "200"), 
-			@ApiResponse(responseCode = "400", 
-				description = "Código da cidade inválido", 
-				content = @Content(schema = @Schema(ref = "ApiError"))), 
-			@ApiResponse(responseCode = "404", 
-				description = "Cidade não encontrada", 
-				content = @Content(schema = @Schema(ref = "ApiError")))
-		})
+	@Operation(summary = "Atualiza uma cidade por código", responses = { @ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "Código da cidade inválido", content = @Content(schema = @Schema(ref = "ApiError"))),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(ref = "ApiError"))) })
 	public CidadeOutputDto atualizar(
 			@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade,
 			@RequestBody(description = "Representação de uma cidade com dados atualizados", required = true) CidadeInputDto cidadeInput);
 
-	@Operation(summary = "Remove uma cidade por código", responses = {
-			@ApiResponse(responseCode = "204"), 
-			@ApiResponse(responseCode = "400", 
-				description = "Código da cidade inválido", 
-				content = @Content(schema = @Schema(ref = "ApiError"))), 
-			@ApiResponse(responseCode = "404", 
-				description = "Cidade não encontrada", 
-				content = @Content(schema = @Schema(ref = "ApiError")))
-		})
+	@Operation(summary = "Remove uma cidade por código", responses = { @ApiResponse(responseCode = "204"),
+			@ApiResponse(responseCode = "400", description = "Código da cidade inválido", content = @Content(schema = @Schema(ref = "ApiError"))),
+			@ApiResponse(responseCode = "404", description = "Cidade não encontrada", content = @Content(schema = @Schema(ref = "ApiError"))) })
 	public void remover(
 			@Parameter(description = "Código de uma cidade", example = "1", required = true) Long codCidade);
 
